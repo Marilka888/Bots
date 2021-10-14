44da0263d1bafbcee6a4b2967a66e76b806d1c47
Marilka888 <marilka888@mail.ru>
2021-10-15 02:23:45 +0300

    Add button change

--- VK/Bot1.0/src/main/java/Main.java
+++ VK/Bot1.0/src/main/java/Main.java
@@ -12,9 +12,26 @@ import com.vk.api.sdk.queries.messages.MessagesGetLongPollHistoryQuery;
 
 public class Main {
     public static void main(String[] args) throws ClientException, ApiException, InterruptedException {
+
         TransportClient transportClient = new HttpTransportClient();//через него передаём запросы
         VkApiClient vk = new VkApiClient(transportClient);//интерфейс взаимодействия с Vk API
         Random random = new Random();
+
+        Keyboard keyboard = new Keyboard();//add keyboard
+
+        List<List<KeyboardButton>> allKey = new ArrayList<>();
+        List<KeyboardButton> line1 = new ArrayList<>();//ряд, куда добавляем кнопки
+
+        //добавляем варианты
+        line1.add(new KeyboardButton().setAction(new KeyboardButtonAction().setLabel("Привет")
+                .setType(KeyboardButtonActionType.TEXT)).setColor(KeyboardButtonColor.POSITIVE));
+        line1.add(new KeyboardButton().setAction(new KeyboardButtonAction().setLabel("Что умеешь?")
+                .setType(KeyboardButtonActionType.TEXT)).setColor(KeyboardButtonColor.POSITIVE));
+
+        allKey.add(line1);//добавляем линию с кнопками
+        keyboard.setButtons(allKey);
+
+
         GroupActor actor = new GroupActor(169265593, "YOUR_TOKEN" );
         Integer ts = vk.messages().getLongPollServer(actor).execute().getTs(); //идентификатор сообщения, с которого начинается обработка сообщений (обновляем)
 
@@ -32,6 +49,10 @@ public class Main {
                         else if (message.getText().equals("Что умеешь?")){
                             vk.messages().send(actor).message("Я многозадачная. Умею плакать и работать одновременно)").userId(message.getFromId()).
                                     randomId(random.nextInt(10000)).execute();
+                        }
+                        else if (message.getText().equals("КНОПКИ ДАЙ")) {
+                            vk.messages().send(actor).message("Ну на").userId(message.getFromId()).
+                                    randomId(random.nextInt(10000)).keyboard(keyboard).execute();
                         }
                         else { //непредусмотренный случай
                             vk.messages().send(actor).message("Что ты высрал?))").userId(message.getFromId()).
